f9022846f70abe01c8041047fe01b8730143e939
Maks Hotson W <hotson@mail.ru>
2016-08-12 10:27:18 +0300

    With start module, compatible with win shortcut

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls;
+  StdCtrls, process;
 
 type
 
@@ -14,8 +14,12 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     TrayIcon1: TTrayIcon;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure FormWindowStateChange(Sender: TObject);
     procedure TrayIcon1Click(Sender: TObject);
   private
@@ -26,6 +30,7 @@ type
 
 var
   Form1: TForm1;
+  AProcess: TProcess;
 
 implementation
 
@@ -43,6 +48,24 @@ procedure TForm1.Button1Click(Sender: TObject);
 begin
   Application.Minimize;
   Form1.Hide;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+//var
+//  AProcess: TProcess;
+begin
+   AProcess := TProcess.Create(nil);
+   AProcess.CommandLine := 'calc.exe';
+//   AProcess.Options := [poRunSuspended, poNoConsole];
+   AProcess.Options := [poNoConsole];
+   AProcess.Execute;
+   AProcess.Free;
+   Application.Terminate;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  AProcess.Free;
 end;
 
 procedure TForm1.TrayIcon1Click(Sender: TObject);
